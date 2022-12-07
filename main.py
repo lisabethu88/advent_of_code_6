1d97c530ae49ec2685b2115c3681f893a3e4ca58
Lisa Utsett <lisabethutsett@gmail.com>
2022-12-06 20:17:21 -0500

    Finished both parts

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+f  = open("input", "r")
+list_same_letters = []
+count = 0
+reset= True
+fourteen_count = 0
+for line in f:
+    line=line.strip()
+    for char in line:
+        list_same_letters = line[count:count+14]
+        count+=1
+        if len(list_same_letters) == len(set(list_same_letters)):
+            count += 13
+            break   
+print(count)
 
